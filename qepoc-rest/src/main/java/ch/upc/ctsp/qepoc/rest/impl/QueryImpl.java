453a234482e8c484bfd1b64551edffc31f804bf9
Andreas König <koa@panter.ch>
2013-09-30 13:04:57 +0200

    Simple Cache added

--- qepoc-rest/src/main/java/ch/upc/ctsp/qepoc/rest/impl/QueryImpl.java
+++ qepoc-rest/src/main/java/ch/upc/ctsp/qepoc/rest/impl/QueryImpl.java
@@ -7,14 +7,15 @@ import java.util.Map;
 
 import ch.upc.ctsp.qepoc.rest.Query;
 import ch.upc.ctsp.qepoc.rest.model.CallbackFuture;
+import ch.upc.ctsp.qepoc.rest.model.CallbackFuture.CallbackHandler;
 import ch.upc.ctsp.qepoc.rest.model.PathDescription;
-import ch.upc.ctsp.qepoc.rest.model.QueryRequest;
-import ch.upc.ctsp.qepoc.rest.model.QueryResult;
 import ch.upc.ctsp.qepoc.rest.model.PathDescription.FixedPathComp;
 import ch.upc.ctsp.qepoc.rest.model.PathDescription.PathComp;
 import ch.upc.ctsp.qepoc.rest.model.PathDescription.VariablePathComp;
+import ch.upc.ctsp.qepoc.rest.model.QueryRequest;
+import ch.upc.ctsp.qepoc.rest.model.QueryResult;
 import ch.upc.ctsp.qepoc.rest.spi.Backend;
-
+import ch.upc.ctsp.qepoc.rest.spi.DirectResult;
 
 /*
  * (c) 2013 panter llc, Zurich, Switzerland.
@@ -25,117 +26,147 @@ import ch.upc.ctsp.qepoc.rest.spi.Backend;
  * 
  */
 public class QueryImpl implements Query {
-  private final FixedListNode rootNode = new FixedListNode(null);
+    private final FixedListNode            rootNode   = new FixedListNode(null);
+    private final Map<String, QueryResult> oldResults = new HashMap<String, QueryResult>();
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see net.cablecom.ctsp.query.Query#query(java.lang.String)
-   */
-  @Override
-  public CallbackFuture<QueryResult> query(final QueryRequest request) {
-    AbstractRegistryNode currentNode = rootNode;
-    final List<String> path = request.getPath();
-    final List<String> parameters = new ArrayList<String>();
-    int pathLength = 0;
-    for (final String pathComp : path) {
-      pathLength += 1;
-      final AbstractRegistryNode nextNode;
-      if (currentNode instanceof FixedListNode) {
-        nextNode = ((FixedListNode) currentNode).getSubNodes().get(pathComp);
-      } else if (currentNode instanceof VariableNode) {
-        nextNode = (AbstractRegistryNode) ((VariableNode) currentNode).getSubNode();
-        parameters.add(pathComp);
-      } else if (currentNode instanceof BackendNode) {
-        return callBackend((BackendNode) currentNode, request, parameters, pathLength - 1);
-      } else {
-        throw new RuntimeException("Unknown Node Type " + currentNode);
-      }
-      if (nextNode == null) {
-        throw new RuntimeException("Cannot resolve Path " + path);
-      }
-      currentNode = nextNode;
-    }
-    return callBackend((BackendNode) currentNode, request, parameters, pathLength);
-  }
-
-  public void registerBackend(final PathDescription path, final Backend backend) {
-    AbstractRegistryNode currentNode = rootNode;
-
-    final PathComp[] components = path.getComponents();
-    final List<String> variableNames = new ArrayList<String>();
-    for (int i = 0; i < components.length; i++) {
-      final PathComp currentPathComp = components[i];
-      final PathComp nextPathComp = i < components.length - 1 ? components[i + 1] : null;
-      if (currentPathComp instanceof FixedPathComp) {
-        final FixedPathComp fixedPathComp = (FixedPathComp) currentPathComp;
-        if (currentNode instanceof FixedListNode) {
-          final Map<String, AbstractRegistryNode> subNodes = ((FixedListNode) currentNode).getSubNodes();
-          final AbstractRegistryNode existingNextNode = subNodes.get(fixedPathComp.getValue());
-          if (existingNextNode != null) {
-            currentNode = existingNextNode;
-            continue;
-          }
-          final AbstractRegistryNode nextNode = createNextNode(nextPathComp, currentNode, backend, variableNames);
-          subNodes.put(fixedPathComp.getValue(), nextNode);
-          currentNode = nextNode;
-        } else {
-          throw new RuntimeException("Error at " + currentNode.describePath() + " while adding path " + path);
+    /*
+     * (non-Javadoc)
+     * 
+     * @see net.cablecom.ctsp.query.Query#query(java.lang.String)
+     */
+    @Override
+    public CallbackFuture<QueryResult> query(final QueryRequest request) {
+        final String key = createKeyFromPath(request.getPath());
+        synchronized (oldResults) {
+            final QueryResult oldResult = oldResults.get(key);
+            if (oldResult != null && oldResult.getCreationDate().after(request.getAllowedSince())) {
+                return new DirectResult<QueryResult>(oldResult);
+            }
         }
-      } else if (currentPathComp instanceof VariablePathComp) {
-        if (currentNode instanceof VariableNode) {
-          final VariableNode variableNode = (VariableNode) currentNode;
-          variableNames.add(((VariablePathComp) currentPathComp).getVariableName());
-          final AbstractRegistryNode existingSubNode = (AbstractRegistryNode) variableNode.getSubNode();
-          if (existingSubNode != null) {
-            currentNode = existingSubNode;
-            continue;
-          }
-          final AbstractRegistryNode newSubNode = createNextNode(nextPathComp, currentNode, backend, variableNames);
-          variableNode.setSubNode(newSubNode);
-          currentNode = newSubNode;
-        } else {
-          throw new RuntimeException("Error at " + currentNode.describePath() + " while adding path " + path);
+        AbstractRegistryNode currentNode = rootNode;
+        final List<String> path = request.getPath();
+        final List<String> parameters = new ArrayList<String>();
+        int pathLength = 0;
+        for (final String pathComp : path) {
+            pathLength += 1;
+            final AbstractRegistryNode nextNode;
+            if (currentNode instanceof FixedListNode) {
+                nextNode = ((FixedListNode) currentNode).getSubNodes().get(pathComp);
+            } else if (currentNode instanceof VariableNode) {
+                nextNode = (AbstractRegistryNode) ((VariableNode) currentNode).getSubNode();
+                parameters.add(pathComp);
+            } else if (currentNode instanceof BackendNode) {
+                return callBackend((BackendNode) currentNode, request, parameters, pathLength - 1);
+            } else {
+                throw new RuntimeException("Unknown Node Type " + currentNode);
+            }
+            if (nextNode == null) {
+                throw new RuntimeException("Cannot resolve Path " + path);
+            }
+            currentNode = nextNode;
         }
-      }
+        return callBackend((BackendNode) currentNode, request, parameters, pathLength);
     }
-  }
 
-  private CallbackFuture<QueryResult> callBackend(final BackendNode backendNode, final QueryRequest request, final List<String> parameters,
-      final int pathLength) {
-    final Map<String, String> variables = createParameterMap(backendNode, parameters, pathLength);
-    final CallbackFuture<QueryResult> result = backendNode.getBackend().query(request, variables, this);
-    return result;
-  }
+    public void registerBackend(final PathDescription path, final Backend backend) {
+        AbstractRegistryNode currentNode = rootNode;
 
-  private AbstractRegistryNode createNextNode(final PathComp nextPathComp, final AbstractRegistryNode currentNode, final Backend backend,
-      final List<String> variableNames) {
-    final AbstractRegistryNode nextNode;
-    if (nextPathComp != null) {
-      if (nextPathComp instanceof FixedPathComp) {
-        nextNode = new FixedListNode(currentNode);
-      } else if (nextPathComp instanceof VariablePathComp) {
-        nextNode = new VariableNode(currentNode);
-      } else {
-        throw new RuntimeException("Unsupported path comp " + nextPathComp);
-      }
-    } else {
-      nextNode = new BackendNode(currentNode, variableNames.toArray(new String[variableNames.size()]), backend);
+        final PathComp[] components = path.getComponents();
+        final List<String> variableNames = new ArrayList<String>();
+        for (int i = 0; i < components.length; i++) {
+            final PathComp currentPathComp = components[i];
+            final PathComp nextPathComp = i < components.length - 1 ? components[i + 1] : null;
+            if (currentPathComp instanceof FixedPathComp) {
+                final FixedPathComp fixedPathComp = (FixedPathComp) currentPathComp;
+                if (currentNode instanceof FixedListNode) {
+                    final Map<String, AbstractRegistryNode> subNodes = ((FixedListNode) currentNode).getSubNodes();
+                    final AbstractRegistryNode existingNextNode = subNodes.get(fixedPathComp.getValue());
+                    if (existingNextNode != null) {
+                        currentNode = existingNextNode;
+                        continue;
+                    }
+                    final AbstractRegistryNode nextNode = createNextNode(nextPathComp, currentNode, backend, variableNames);
+                    subNodes.put(fixedPathComp.getValue(), nextNode);
+                    currentNode = nextNode;
+                } else {
+                    throw new RuntimeException("Error at " + currentNode.describePath() + " while adding path " + path);
+                }
+            } else if (currentPathComp instanceof VariablePathComp) {
+                if (currentNode instanceof VariableNode) {
+                    final VariableNode variableNode = (VariableNode) currentNode;
+                    variableNames.add(((VariablePathComp) currentPathComp).getVariableName());
+                    final AbstractRegistryNode existingSubNode = (AbstractRegistryNode) variableNode.getSubNode();
+                    if (existingSubNode != null) {
+                        currentNode = existingSubNode;
+                        continue;
+                    }
+                    final AbstractRegistryNode newSubNode = createNextNode(nextPathComp, currentNode, backend, variableNames);
+                    variableNode.setSubNode(newSubNode);
+                    currentNode = newSubNode;
+                } else {
+                    throw new RuntimeException("Error at " + currentNode.describePath() + " while adding path " + path);
+                }
+            }
+        }
     }
-    return nextNode;
-  }
 
-  private Map<String, String> createParameterMap(final BackendNode backendNode, final List<String> parameters, final int pathLength) {
-    final String[] variableNames = backendNode.getVariableEntries();
-    assert variableNames.length == parameters.size();
-    final Map<String, String> variables = new HashMap<String, String>();
-    variables.put("match-length", Integer.toString(pathLength));
-    for (int i = 0; i < variableNames.length; i++) {
-      final String name = variableNames[i];
-      final String value = parameters.get(i);
-      variables.put(name, value);
+    private CallbackFuture<QueryResult> callBackend(final BackendNode backendNode, final QueryRequest request, final List<String> parameters,
+            final int pathLength) {
+        final Map<String, String> variables = createParameterMap(backendNode, parameters, pathLength);
+        final CallbackFuture<QueryResult> result = backendNode.getBackend().query(request, variables, this);
+        result.registerCallback(new CallbackHandler<QueryResult>() {
+
+            @Override
+            public void handleException(final Throwable exception) {
+            }
+
+            @Override
+            public void handleValue(final QueryResult value) {
+                final String key = createKeyFromPath(request.getPath());
+                synchronized (oldResults) {
+                    final QueryResult cachedResult = oldResults.get(key);
+                    if (cachedResult == null || cachedResult.getCreationDate().before(value.getCreationDate())) {
+                        oldResults.put(key, value);
+                    }
+                }
+            }
+        });
+        return result;
     }
-    return variables;
-  }
+
+    private String createKeyFromPath(final List<String> path) {
+        final String key = path.toString();
+        return key;
+    }
+
+    private AbstractRegistryNode createNextNode(final PathComp nextPathComp, final AbstractRegistryNode currentNode, final Backend backend,
+            final List<String> variableNames) {
+        final AbstractRegistryNode nextNode;
+        if (nextPathComp != null) {
+            if (nextPathComp instanceof FixedPathComp) {
+                nextNode = new FixedListNode(currentNode);
+            } else if (nextPathComp instanceof VariablePathComp) {
+                nextNode = new VariableNode(currentNode);
+            } else {
+                throw new RuntimeException("Unsupported path comp " + nextPathComp);
+            }
+        } else {
+            nextNode = new BackendNode(currentNode, variableNames.toArray(new String[variableNames.size()]), backend);
+        }
+        return nextNode;
+    }
+
+    private Map<String, String> createParameterMap(final BackendNode backendNode, final List<String> parameters, final int pathLength) {
+        final String[] variableNames = backendNode.getVariableEntries();
+        assert variableNames.length == parameters.size();
+        final Map<String, String> variables = new HashMap<String, String>();
+        variables.put("match-length", Integer.toString(pathLength));
+        for (int i = 0; i < variableNames.length; i++) {
+            final String name = variableNames[i];
+            final String value = parameters.get(i);
+            variables.put(name, value);
+        }
+        return variables;
+    }
 
 }
