80fff5fa6ca549aeb41f145c45466fe1355313fa
Andreas König <koa@panter.ch>
2013-09-30 12:53:08 +0200

    Working poc

---
+++ qepoc-rest/src/test/java/ch/upc/ctsp/qepoc/rest/poc/PocMain.java
@@ -0,0 +1,78 @@
+/*
+ * (c) 2013 panter llc, Zurich, Switzerland.
+ */
+package ch.upc.ctsp.qepoc.rest.poc;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.commons.lang.StringUtils;
+
+import ch.upc.ctsp.qepoc.client.Client;
+import ch.upc.ctsp.qepoc.rest.Query;
+import ch.upc.ctsp.qepoc.rest.alias.Alias;
+import ch.upc.ctsp.qepoc.rest.alias.Alias.Builder;
+import ch.upc.ctsp.qepoc.rest.impl.QueryImpl;
+import ch.upc.ctsp.qepoc.rest.model.CallbackFuture;
+import ch.upc.ctsp.qepoc.rest.model.PathDescription;
+import ch.upc.ctsp.qepoc.rest.model.QueryRequest;
+import ch.upc.ctsp.qepoc.rest.model.QueryResult;
+import ch.upc.ctsp.qepoc.rest.spi.Backend;
+import ch.upc.ctsp.qepoc.rest.spi.DirectResult;
+
+/**
+ * TODO: add type comment.
+ * 
+ */
+public class PocMain {
+    public static void main(final String args[]) throws IOException, InterruptedException, ExecutionException {
+        final QueryImpl query = new QueryImpl();
+        query.registerBackend(PathDescription.createFromString("mock"), new Backend() {
+
+            @Override
+            public CallbackFuture<QueryResult> query(final QueryRequest request, final Map<String, String> parameters, final Query executingQuery) {
+                final List<String> path = request.getPath();
+                final List<String> remainingPath = path.subList(Integer.parseInt(parameters.get("match-length")), path.size());
+                try {
+                    final String result = Client.query("/" + StringUtils.join(remainingPath, '/'));
+                    return new DirectResult<QueryResult>(new QueryResult(result.trim()));
+                } catch (final Throwable e) {
+                    throw new RuntimeException("Cannot call " + remainingPath, e);
+                }
+            }
+        });
+        query.registerBackend(PathDescription.createFromString("modem/{mac}/ip"), new Alias.Builder().addConstEntry("mock").addConstEntry("modem")
+                .addVariableEntry("mac").addConstEntry("ip").build());
+
+        final Builder modemScopeBuilder = new Alias.Builder().addConstEntry("mock").addConstEntry("scopes");
+        modemScopeBuilder.createSubpath().addConstEntry("modem").addVariableEntry("mac").addConstEntry("ip");
+        query.registerBackend(PathDescription.createFromString("modem/{mac}/scope"), modemScopeBuilder.build());
+
+        final Builder cmtsNameBuilder = new Alias.Builder().addConstEntry("mock").addConstEntry("scopes");
+        cmtsNameBuilder.createSubpath().addConstEntry("modem").addVariableEntry("mac").addConstEntry("scope");
+        cmtsNameBuilder.addConstEntry("cmts");
+        query.registerBackend(PathDescription.createFromString("modem/{mac}/cmts/name"), cmtsNameBuilder.build());
+
+        final Builder cmIdBuilder = new Alias.Builder().addConstEntry("mock").addConstEntry("cmts");
+        cmIdBuilder.createSubpath().addConstEntry("modem").addVariableEntry("mac").addConstEntry("cmts").addConstEntry("name");
+        cmIdBuilder.addConstEntry("modem").addVariableEntry("mac").addConstEntry("cmId");
+        query.registerBackend(PathDescription.createFromString("modem/{mac}/cmts/cmId"), cmIdBuilder.build());
+
+        final Builder cmtsIpBuilder = new Alias.Builder().addConstEntry("mock").addConstEntry("cmts");
+        cmtsIpBuilder.createSubpath().addConstEntry("modem").addVariableEntry("mac").addConstEntry("cmts").addConstEntry("name");
+        cmtsIpBuilder.addConstEntry("ip");
+        query.registerBackend(PathDescription.createFromString("modem/{mac}/cmts/ip"), cmtsIpBuilder.build());
+
+        final Builder upstreamIfcBuilder = new Alias.Builder().addConstEntry("mock").addConstEntry("snmp");
+        upstreamIfcBuilder.createSubpath().addConstEntry("modem").addVariableEntry("mac").addConstEntry("cmts").addConstEntry("ip");
+        upstreamIfcBuilder.createPatternEntry("1.3.6.2.1.2.3.4.{0}").createSubpath().addConstEntry("modem").addVariableEntry("mac")
+                .addConstEntry("cmts").addConstEntry("cmId");
+        query.registerBackend(PathDescription.createFromString("modem/{mac}/mainUpstreamIfc"), upstreamIfcBuilder.build());
+
+        final CallbackFuture<QueryResult> response = query.query(QueryRequest.createRequest("modem/00AB123456/mainUpstreamIfc"));
+        System.out.println("------------");
+        System.out.println(response.get().getValue());
+    }
+}
